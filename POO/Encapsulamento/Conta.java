166a2c80797e5c3d6999e3bc835dd305e2ed86a4
José Guilherme da S. Neto <165957761+ohZeka43@users.noreply.github.com>
2025-08-11 14:59:58 -0300

    Update Conta.java

--- POO/Encapsulamento/Conta.java
+++ POO/Encapsulamento/Conta.java
@@ -2,12 +2,12 @@ public class Conta {
     private String titular;
     private double saldo;
 
-    // Construtor correto
+    //////////////////////////////////////////////////////
     public Conta(String titular, double saldoInicial) {
         this.titular = titular;
         this.saldo = saldoInicial;
     }
-
+    /////////////////////////////////////////////////////
     public void depositar(double valor) {
         if (valor >= 0) {
             saldo += valor;
@@ -15,7 +15,7 @@ public class Conta {
             System.out.println("Valor inválido para depósito");
         }
     }
-
+    /////////////////////////////////////////////////////////
     public void sacar(double valor) {
         if (valor <= saldo) {
             saldo -= valor;
@@ -23,11 +23,11 @@ public class Conta {
             System.out.println("Saldo insuficiente");
         }
     }
-
+    ////////////////////////////////////////////////////////////////
     public void exibirSaldo() {
         System.out.println("Titular: " + titular + " | Saldo da conta: " + saldo);
     }
-
+    //////////////////////////////////////////////////////////////////////
     public static void main(String[] args) {
         Conta c = new Conta("João", 0);
         c.depositar(800);
