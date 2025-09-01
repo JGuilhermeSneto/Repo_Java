f0aeea94c723472f23af557247f1a270e4d01e23
José Guilherme da S. Neto <165957761+JGuilhermeSneto@users.noreply.github.com>
2025-09-01 14:21:11 -0300

    Exercicios de Lista

--- Estrutura_de_dados/Estrutueas_lineares/Lista/Exercicio5.java
+++ Estrutura_de_dados/Estrutueas_lineares/Lista/Exercicio5.java
@@ -16,11 +16,12 @@ public class Exercicio5 {
         Lista();
 
         int aux;
-        for (int i = 0; i < tamanho - 1; i++) {           // vai até tamanho-2
-            for (int j = 0; j < tamanho - 1 - i; j++) {   // vai até tamanho-2-i
+        for (int i = 0; i < tamanho - 1; i++) {          
+            for (int j = 0; j < tamanho - 1 - i; j++) {   
                 if (lista[j] > lista[j + 1]) {
                     aux = lista[j];
                     lista[j] = lista[j + 1];
+                    
                     lista[j + 1] = aux;
                 }
             }
